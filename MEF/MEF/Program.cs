c0ccac2981d41231b983c1e0ccb4e92e0872fcf0
Anton_Suprun <anton.suprun@hotmail.com>
2013-10-31 14:20:01 +0300

    mef framework

---
+++ MEF/MEF/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using SimpleCalculator;
+
+namespace MEF
+{
+    class Program
+    {
+        private CompositionContainer container;
+
+        [Import(typeof(ICalculator))]
+        public ICalculator Calculator{get;set;}
+
+        private Program()
+        {
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+            catalog.Catalogs.Add(new DirectoryCatalog("D:\\Study\\BurgerTraining\\MEF\\MEF\\Extensions"));
+            container = new CompositionContainer(catalog);
+            
+            try
+            {
+                this.container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+                throw;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program(); 
+            String s;
+            
+            Console.WriteLine("Enter Command:");
+            while (true)
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(p.Calculator.Calculate(s));
+            }            
+        }
+    }
+}
---
+++ MEF/MEF/SimpleCalculator/MySimpleCalculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace SimpleCalculator
+{
+    [Export(typeof(ICalculator))]
+    public class MySimpleCalculator : ICalculator
+    {
+        public String Calculate(String input)
+        { 
+            int fn = FindFirstNonDigit(input);
+            
+            if (fn < 0)
+            {
+                return "Could not parse command.";
+            }
+            int left;
+            int right;
+            try
+            {
+                left = int.Parse(input.Substring(0, fn));
+                right = int.Parse(input.Substring(fn + 1));
+            }
+            catch 
+            {
+                return "Could not parse command.";
+            }
+            Char operation = input[fn];
+            String result = "Operation Not Found!";
+            foreach (Lazy<IOperation, IOperationData> currentOperation in operations)
+            {
+                if (currentOperation.Metadata.Symbol.Equals(operation))
+                {
+                    result = currentOperation.Value.Operate(left, right).ToString();
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private int FindFirstNonDigit(String s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!(Char.IsDigit(s[i]))) return i;
+            }
+            return -1;
+        }
+        
+
+        [ImportMany]
+        private IEnumerable<Lazy<IOperation, IOperationData>> operations;
+    }
+}
