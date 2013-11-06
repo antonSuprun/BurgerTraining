b575da88f19a77b86556ab859d0ff8c3f93abd45
Anton_Suprun <anton.suprun@hotmail.com>
2013-11-06 16:07:29 +0300

    Converter

---
+++ WPF/pluralsight course/DataConverterDemo/DataConverterDemo/DateConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace DataConverterDemo
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime)value;
+            return date.ToString("d");
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            String strValue = value as String;
+            DateTime resultDateTime;
+            
+            if (!DateTime.TryParse(strValue, out resultDateTime))
+            {
+                throw new Exception("Unable to convert string to date time");    
+            }
+            
+            return resultDateTime;
+        }
+    }
+}
