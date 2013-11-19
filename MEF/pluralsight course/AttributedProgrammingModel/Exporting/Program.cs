f079127bb0cb48b6e14f1f7f77e076ea261b4724
Anton Suprun <anton.suprun@hotmail.com>
2013-11-19 12:56:44 +0300

    MEF exporting

---
+++ MEF/pluralsight course/AttributedProgrammingModel/Exporting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+
+namespace Exporting
+{
+    [InheritedExport]
+    public interface IClass { }
+
+    public class ClassA : IClass { }
+
+    public class ClassB : ClassA { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var catalog = new AssemblyCatalog(typeof(Program).Assembly);
+            var container = new CompositionContainer(catalog);
+
+            foreach (var part in container.Catalog.Parts)
+            {
+                Console.WriteLine(part.ToString());
+            }
+        }
+    }
+}
