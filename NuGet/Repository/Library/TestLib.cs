e5b07ca7658a52b7b85f24e2dc47a593afed2bf5
Anton Suprun <anton.suprun@hotmail.com>
2014-01-03 11:34:59 +0300

    NuGet Repository

---
+++ NuGet/Repository/Library/TestLib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class TestLib
+    {
+        public String SayHallo() 
+        {
+            return "Hallo from Library";
+        }
+    }
+}
